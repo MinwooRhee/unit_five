dcd918da0c5bf93854419e0775d402930ace3d1f
Min Woo <minwoo@iMac5.local>
2018-10-16 11:32:36 -0400

    infinite loops can be good

---
+++ d4_unit_five_warmups.py
@@ -0,0 +1,9 @@
+
+while True:
+    number = int(input("Please type in a number between 1 - 100: "))
+    if number >= 1 and number <= 100:
+        break
+    else:
+        print("That is not valid")
+
+print("You did it!")
